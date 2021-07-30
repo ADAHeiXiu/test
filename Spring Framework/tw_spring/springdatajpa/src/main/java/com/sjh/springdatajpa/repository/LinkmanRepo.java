32d1daaaf3519507d0a50a5a03efa05ba46e8067
ADA <72425038+ADAHeiXiu@users.noreply.github.com>
2021-07-30 09:30:54 +0800

    Delete Spring Framework directory

--- Spring Framework/tw_spring/springdatajpa/src/main/java/com/sjh/springdatajpa/repository/LinkmanRepo.java
+++
@@ -1,7 +0,0 @@
-package com.sjh.springdatajpa.repository;
-
-import com.sjh.springdatajpa.entity.Linkman;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkmanRepo extends JpaRepository<Linkman,Long> {
-}
--- Spring Framework/tw_spring/springdatajpa/src/main/java/com/sjh/springdatajpa/repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.sjh.springdatajpa.repository;
-
-import com.sjh.springdatajpa.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-}
