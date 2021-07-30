91d4068bd24eb8bb6b713839e84cb2662c3e1c1d
ADA <72425038+ADAHeiXiu@users.noreply.github.com>
2021-07-30 09:31:21 +0800

    Delete VueProjects directory

--- VueProjects/springboot/src/main/java/com/sjh/springboot/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sjh.springboot.repository;
-
-import com.sjh.springboot.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock,Integer> {
-}
